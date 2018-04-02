27c49bccb6f701544d0e371d3d287dda778d20f0
Christian Budde Christensen <budde377@gmail.com>
2018-04-02 13:48:39 +0200

    feat: Reintroduce email

--- packages/api/src/middleware/image.js
+++ packages/api/src/middleware/image.js
@@ -1,16 +1,19 @@
 import KoaRouter from 'koa-router'
 import compose from 'koa-compose'
-import { userFromId } from '../db'
+import { user, userFromId } from '../db'
 import type { User } from '../db'
 import { identiconFromString } from '../util/identicon'
 
 const router = new KoaRouter()
 
 function userPicture (uf: (s: string) => Promise<?User>, f: (ctx: *) => string) {
-  return async (ctx) => {
+  return async (ctx, next) => {
     const id = f(ctx)
     const u = await uf(id)
-    const {data, mime, fetched} = (u && u.picture) || identiconFromString(id)
+    if (!u) {
+      next()
+    }
+    const {data, mime, fetched} = u.picture || identiconFromString(id)
     ctx.body = data
     ctx.type = mime
     ctx.set('ETag', fetched.toString())
@@ -18,5 +21,7 @@ function userPicture (uf: (s: string) => Promise<?User>, f: (ctx: *) => string) 
 }
 
 router.get('/i/id/:id', userPicture(userFromId, ctx => ctx.params.id))
+
+router.get('/i/email/:email', userPicture(user, ctx => ctx.params.email))
 
 export default compose([router.routes(), router.allowedMethods()])
